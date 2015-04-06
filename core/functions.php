16156a4b48d42c1a630f9a6626f52b3b739be1db
Baptiste <boruezabal@gmail.com>
2015-04-06 12:59:09 +0100

    Modification de la fonction generate_head pour gérer les scripts javascripts

--- core/functions.php
+++ core/functions.php
@@ -1,14 +1,24 @@
 <?php
 
-function generate_head($page_title)
+function generate_head($page_title, $js = NULL)
 {
 	echo "<!DOCTYPE html>
 <html lang=\"fr\">
     <head>
         <meta charset=\"UTF-8\" />
         <title>Calendar - ".$page_title."</title>
-            <link rel=\"stylesheet\" href=\"../css/bootstrap.css\" />
-    </head>
+            <link rel=\"stylesheet\" href=\"../css/bootstrap.css\" />";
+    
+    echo '<script type="text/javascript" src="../js/jquery.js"></script>'."\n";
+    if (!is_null($js))
+    {
+    	foreach ($js as $script) {
+    		$script = str_replace("js/", "", $script);
+    		echo '<script type="text/javascript" src="../js/' .$script. '.js"></script>'."\n";
+    	}
+    }
+    
+    echo "</head>
     <body>";
 }
 
