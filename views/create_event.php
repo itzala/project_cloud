86f00d580cc0d44c4e651a982568ace30316172a
Bigpurple52 <finalfantasy_cristalchronicle@hotmail.com>
2015-04-07 13:48:04 +0100

    Add css to the form

--- views/create_event.php
+++ views/create_event.php
@@ -11,41 +11,46 @@ generate_head("Create an event");
 		<a href="./" class="btn btn-primary">Back to index</a>
 	</nav>
 
-	<form>
-		<div class="form-group">
-			<label for="event_name">Name : </label>
-			<input id="event_name" type="text" class="" name="name"/>
+	<div class="row">
+		<div class="col-xs-1 col-sm-2 col-md-2 col-lg-6 col-lg-offset-3">
+			<h1>Create an event</h1><hr />
+			<form role="form" id="form_create_event">
+				<div class="form-group">
+					<label class="control-label" for="event_name">Name : </label>
+					<input class="form-control" id="event_name" type="text" name="name"/>
+				</div>
+				<div class="form-group">
+					<label class="control-label" for="date_event">Date : </label>
+					<input class="form-control" id="date_event" type="text" name="name"/>
+				</div>
+				<div class="form-group">
+					<label class="control-label" for="event_name">Event name : </label>
+					<input class="form-control" id="event_name" type="text" name="name"/>
+				</div>
+				<div class="form-group">
+					<label class="control-label" for="event_name">Event name : </label>
+					<input class="form-control" id="event_name" type="text" name="name"/>
+				</div>
+				<div class="form-group">
+					<label class="control-label" for="event_name">Event name : </label>
+					<input class="form-control" id="event_name" type="text" name="name"/>
+				</div>
+				<div class="form-group">
+					<label class="control-label" for="event_name">Event name : </label>
+					<input class="form-control" id="event_name" type="text" name="name"/>
+				</div>
+				<div class="form-group">
+					<label class="control-label" for="event_name">Event name : </label>
+					<input class="form-control" id="event_name" type="text" name="name"/>
+				</div>
+				<div class="form-group">
+					<label class="control-label" for="event_name">Event name : </label>
+					<input class="form-control" id="event_name" type="text" name="name"/>
+				</div>
+				<input class="form-control btn btn-success" type="submit" value="Create"/>
+			</form>
 		</div>
-		<div class="form-group">
-			<label for="date_event">Date : </label>
-			<input id="date_event" type="text" class="" name="name"/>
-		</div>
-		<div class="form-group">
-			<label for="event_name">Event name : </label>
-			<input id="event_name" type="text" class="" name="name"/>
-		</div>
-		<div class="form-group">
-			<label for="event_name">Event name : </label>
-			<input id="event_name" type="text" class="" name="name"/>
-		</div>
-		<div class="form-group">
-			<label for="event_name">Event name : </label>
-			<input id="event_name" type="text" class="" name="name"/>
-		</div>
-		<div class="form-group">
-			<label for="event_name">Event name : </label>
-			<input id="event_name" type="text" class="" name="name"/>
-		</div>
-		<div class="form-group">
-			<label for="event_name">Event name : </label>
-			<input id="event_name" type="text" class="" name="name"/>
-		</div>
-		<div class="form-group">
-			<label for="event_name">Event name : </label>
-			<input id="event_name" type="text" class="" name="name"/>
-		</div>
-		<input type="submit" value="Create" class="btn btn-success"/>
-	</form>
+	</div>
 
 
 <?php
