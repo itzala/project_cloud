7a3e1f8dbb431079a14607f775f7a2c34c63178a
Baptiste <boruezabal@gmail.com>
2015-04-30 20:20:22 +0100

    Prise en compte des heures et du offset dans le js (parametres GET pour creer l'event)

--- js/event.js
+++ js/event.js
@@ -1,5 +1,10 @@
 $(function(){
-	$("#event-table td").click(function(){
-		window.location = "./create_event.php";
+
+	var current_date = new Date();
+
+	$("#event-table td").click(function(){		
+		var day = $(this).attr("data_day");
+		var time = $(this).attr("data_time");
+		window.location = "./create_event.php?d="+day+"&t="+time;		
 	});
 });
